6dc3dfef5f9d4583192a4eb1d621cf59cd560e99
Georg Westner <georg.westner@googlemail.com>
2022-04-04 14:36:37 +0200

    revert Java version to 1.8 && Create TimeController with working TimeEntry retrieve from external docker

--- src/main/java/georg/westner/java_react/TimeController.java
+++ src/main/java/georg/westner/java_react/TimeController.java
@@ -1,17 +1,35 @@
 package georg.westner.java_react;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 class TimeController {
 
-    @GetMapping(value="/hello", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String sayHello() {
+    private static final Logger log = LoggerFactory.getLogger(TimeController.class);
 
-        return "Hello there!";
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
-    // end::get-aggregate-root[]
 
+    @GetMapping(value="/time")
+    TimeEntry[] getTimeByEmail() {
+        RestTemplate restTemplate = new RestTemplate();
+        String timeResourceUrl
+                = "http://0.0.0.0:8080/records?email=aliriza.saral@gmail.com";
+        TimeEntry[] entries
+                = restTemplate.getForObject(timeResourceUrl, TimeEntry[].class);
+        return entries;
+    }
 }
---
+++ src/main/java/georg/westner/java_react/TimeEntry.java
@@ -0,0 +1,48 @@
+package georg.westner.java_react;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class TimeEntry {
+
+    private String email;
+    private String start;
+    private String end;
+
+    public TimeEntry(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Time Entry: " +
+                "email=" + email + "'\'" +
+                "start=" + start  + "'\'" +
+                "end=" + end;
+    }
+}
